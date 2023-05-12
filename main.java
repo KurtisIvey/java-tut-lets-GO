f45d05c4b9f56e136aabb355e22333c40a902105
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-11 20:34:45 -0500

    int scope lesson

--- main.java
+++ main.java
@@ -1,5 +1,15 @@
-class Main {
-    public static void main(String[] args) {
+/* class Main {
+    public static void main(String[] arguments) {
         System.out.println("Hello world!");
+    }
+} */
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int age = 27;
+        // redeclared variables cause error
+        // int age = 17;
+
+        System.out.println("I am " + age + " years old.");
     }
 }
