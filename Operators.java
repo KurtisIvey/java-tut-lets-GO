9c0b630a78dc181de940201d354e91d5ad8cfb58
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-11 22:10:05 -0500

    finish operator section

---
+++ Operators.java
@@ -0,0 +1,46 @@
+class Operators {
+    public static void main(String[] args) {
+        /*
+         * int number1 = 12;
+         * int number2 = 6;
+         * 
+         * // addition
+         * System.out.println(number1 + number2);
+         * 
+         * // subtraction
+         * System.out.println(number1 - number2);
+         * 
+         * // multiplication
+         * System.out.println(number1 * number2);
+         * 
+         * // division , no remainder unless double present
+         * // if int divided by double or float vise versa, always another double
+         * System.out.println(number1 / number2);
+         * 
+         * // remainder (modulo/modulus)
+         * System.out.println(number1 % number2);
+         */
+
+        int number1 = 12;
+        int number2 = 15;
+
+        // is equal to
+        System.out.println(number1 == number2);
+
+        // is not equal to
+        System.out.println(number1 != number2);
+
+        // is greater than
+        System.out.println(number1 > number2);
+
+        // is less than
+        System.out.println(number1 < number2);
+
+        // is greater than or equal to
+        System.out.println(number1 >= number2);
+
+        // is less than or equal to
+        System.out.println(number1 <= number2);
+
+    }
+}
